e5568ace05605de964a71bf053408534d3e47ba6
KingCorvid <mario.arce0812@gmail.com>
2024-12-15 16:47:48 -0500

    Player takes damage from tile hazards
    damage component for tile hazards has been changed to a percentage of max health in represented as a decimal

--- interact/hurtbox.gd
+++ interact/hurtbox.gd
@@ -11,13 +11,16 @@ func _ready() -> void:
 	self.body_entered.connect(hurtbox_body_entered)
 
 func hazard_damage():
+	var map_pos = hazardmap.local_to_map(hazardmap.to_local(global_position))
+	print(hazardmap.get_cell_tile_data(map_pos).get_custom_data("damage"))
+	health_component.direct_damage(health_component.max_health * hazardmap.get_cell_tile_data(map_pos).get_custom_data("damage"))
 	hazard_delay_timer.start()
 
 func hurtbox_body_entered(body):
 	#checks if intersecting any hazard elements on the tilemap and updates the hazardmap node
 	if body.is_in_group("Map"):
 		hazardmap = body
-		if hazard_delay_timer.paused == true:
+		if hazard_delay_timer.time_left == 0:
 			hazard_delay_timer.start()
 
 func damage(attack:Attack):
