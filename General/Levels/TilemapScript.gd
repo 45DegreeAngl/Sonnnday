2c6cab3cd6e336651517dff252bd29d8202dbfe4
KingCorvid <mario.arce0812@gmail.com>
2024-12-15 14:28:58 -0500

    Added Basic TIlemap
    Added a test version of the Level 1 tilemap with collision set up for walls and lava
    Important notes
    -movement colision is handled on layer 1 which is physics layer 0 on the tilemap
    -persistent hazard damage like lava is handled on layer 3 which is physics layer 1 on the tilemap
    -persistent hazard damage uses the custom data layer called damage to tell how much damage is dealt when stepping on them

--- General/Levels/TilemapScript.gd
+++ General/Levels/TilemapScript.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var player_scene = load("res://character/player.tscn")
+	var player_instance = player_scene.instantiate()
+	add_child(player_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
