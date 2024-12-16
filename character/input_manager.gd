6949e418c6ee0fe3754a6922eab3a3a2a87375cb
Gabriel Reyes Wagner <gabrielreyeswagner@gmail.com>
2024-12-15 19:12:21 -0500

    3 players working!

--- character/input_manager.gd
+++ character/input_manager.gd
@@ -3,6 +3,7 @@ class_name InputManager
 extends Node
 
 @export var playerIndex : int = -1
+@export var deviceIndex : int = -1
 var moveDirection: Vector2 = Vector2.ZERO
 var lookDirection: Vector2 = Vector2.ZERO
 var aPressed: bool = false
@@ -14,19 +15,20 @@ func _ready() -> void:
 		#if this player data is connected to this player instance
 		if Globals.players[i].player == get_parent():
 			playerIndex = i+1
+			deviceIndex = Globals.players[i].deviceIndex+1
 	if playerIndex == -1:
 		push_warning("This player wasn't assigned an input device!")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var move_horz: float = Input.get_action_strength("move_right_p" + str(playerIndex)) - Input.get_action_strength("move_left_p" + str(playerIndex))
-	var move_vert: float = Input.get_action_strength("move_down_p" + str(playerIndex)) - Input.get_action_strength("move_up_p" + str(playerIndex))
+	var move_horz: float = Input.get_action_strength("move_right_p" + str(deviceIndex)) - Input.get_action_strength("move_left_p" + str(deviceIndex))
+	var move_vert: float = Input.get_action_strength("move_down_p" + str(deviceIndex)) - Input.get_action_strength("move_up_p" + str(deviceIndex))
 	moveDirection = Vector2(move_horz, move_vert).normalized()
 	
-	var look_horz: float = Input.get_action_strength("right_stick_right_p" + str(playerIndex)) - Input.get_action_strength("right_stick_left_p" + str(playerIndex))
-	var look_vert: float = Input.get_action_strength("right_stick_up_p" + str(playerIndex)) - Input.get_action_strength("right_stick_down_p" + str(playerIndex))
+	var look_horz: float = Input.get_action_strength("right_stick_right_p" + str(deviceIndex)) - Input.get_action_strength("right_stick_left_p" + str(deviceIndex))
+	var look_vert: float = Input.get_action_strength("right_stick_up_p" + str(deviceIndex)) - Input.get_action_strength("right_stick_down_p" + str(deviceIndex))
 	lookDirection = Vector2(look_horz, look_vert).normalized()
 	
-	aPressed = Input.is_action_pressed("a_p" + str(playerIndex))
-	bPressed = Input.is_action_pressed("b_p" + str(playerIndex))
+	aPressed = Input.is_action_pressed("a_p" + str(deviceIndex))
+	bPressed = Input.is_action_pressed("b_p" + str(deviceIndex))
